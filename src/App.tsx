198c4d747f2805130b93a84da6a12a9764bfebf4
noel-jose <noeljosetp@gmail.com>
2022-03-18 13:48:23 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import AppContainer from "./AppContainer";
+
+const formFields = [
+  { id: 1, label: "First Name", input: "text" },
+  { id: 2, label: "Last Name", input: "text" },
+  { id: 3, label: "Email ", input: "text" },
+  // { id: 4, label: "Phone Number ", input: "text" },
+  // { id: 5, label: "Date of Birth", input: "text" },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div className="p-4 mx-auto bg-white shadow-lg rounded-xl">
+        <Header
+          title={"Welcome to Lesson 5 of $react-typescript with #tailwind"}
+        />
+        {formFields.map((field) => (
+          <React.Fragment key={field.id}>
+            <label className="m-2" htmlFor="">
+              {field.label}
+            </label>
+            <input
+              className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
+              type="text"
+            />
+          </React.Fragment>
+        ))}
+      </div>
+    </AppContainer>
   );
 }
 

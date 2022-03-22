16a557b61c056629a9892c2f03d1a154894e3157
noel-jose <noeljosetp@gmail.com>
2022-03-22 22:47:55 +0530

    Added clear form feature

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
+import { setConstantValue } from "typescript";
 import FormField from "./FormField";
 
 const formFields = [
-  { id: 1, label: "First Name", input: "text" },
-  { id: 2, label: "Last Name", input: "text" },
-  { id: 3, label: "Email ", input: "email" },
-  { id: 4, label: "Phone Number ", input: "number" },
-  { id: 5, label: "Date of Birth", input: "date" },
+  { id: 1, label: "First Name", input: "text", value: "" },
+  { id: 2, label: "Last Name", input: "text", value: "" },
+  { id: 3, label: "Email ", input: "email", value: "" },
+  { id: 4, label: "Phone Number ", input: "number", value: "" },
+  { id: 5, label: "Date of Birth", input: "date", value: "" },
 ];
 
 export default function Form(props: { closeFormCB: () => void }) {
   const [state, setState] = useState(formFields);
   const [newField, setNewField] = useState("");
   const addField = () => {
+    // console.log("dfa");
     setState([
       ...state,
-      { id: Number(Date.now()), label: newField, input: "text" },
+      { id: Number(Date.now()), label: newField, input: "text", value: "" },
     ]);
     setNewField("");
   };
@@ -24,11 +26,30 @@ export default function Form(props: { closeFormCB: () => void }) {
     setState(state.filter((field) => field.id !== id));
   };
 
+  const clearForm = () => {
+    setState(state.map((field) => ({ ...field, value: " " })));
+  };
+
+  const changeField = (id: number, value: string) => {
+    setState(
+      state.map((field) => {
+        if (field.id === id) {
+          return { ...field, value: value };
+        }
+        return field;
+      })
+    );
+  };
   return (
     <div className="flex flex-col gap-2 p-4 divide-y-2 divide-dotted">
       <div>
         {state.map((field) => (
-          <FormField key={field.id} field={field} removeFieldCB={removeField} />
+          <FormField
+            key={field.id}
+            field={field}
+            removeFieldCB={removeField}
+            changeFieldCB={changeField}
+          />
         ))}
       </div>
       <div className="flex">
@@ -55,6 +76,12 @@ export default function Form(props: { closeFormCB: () => void }) {
           onClick={props.closeFormCB}
         >
           Close Form
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 my-4 font-bold rounded-lg"
+          onClick={clearForm}
+        >
+          Clear Form
         </button>
       </div>
     </div>
--- src/Components/FormField.tsx
+++ src/Components/FormField.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FormField(props: {
-  field: { id: number; label: string; input: string };
+  field: { id: number; label: string; input: string; value: string };
   removeFieldCB: (id: number) => void;
+  changeFieldCB: (key: number, value: string) => void;
 }) {
   return (
     <React.Fragment>
@@ -11,8 +12,12 @@ export default function FormField(props: {
       </label>
       <div className="flex gap-2">
         <input
+          value={props.field.value}
           className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full focus:outline-blue-500 flex-1"
           type={props.field.input}
+          onChange={(e) => {
+            props.changeFieldCB(props.field.id, e.target.value);
+          }}
         />
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 my-4 font-bold rounded-lg"

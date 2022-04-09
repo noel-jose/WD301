622b12282017803981245516de0277b724480043
noel-jose <noeljosetp@gmail.com>
2022-04-10 05:03:13 +0530

    Changed the input type in PreviewField

--- src/Components/PreviewField.tsx
+++ src/Components/PreviewField.tsx
@@ -12,7 +12,7 @@ export default function PreviewField(props: {
           name="fieldInput"
           value={props.field.value}
           className="border-2 border-gray-200 rounded-lg p-2 m-1 w-full focus:outline-blue-500 flex-1"
-          type="text"
+          type={props.field.inputtype}
           onChange={(e) => {
             props.changeFieldCB(props.field.id, e.target.value);
           }}
